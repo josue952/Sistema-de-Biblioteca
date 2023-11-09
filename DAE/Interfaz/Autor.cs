9a443c1d4ec47e9d0ad0ec5736066086e781d7c0
josue952 <147934757+josue952@users.noreply.github.com>
2023-11-08 22:38:49 -0600

    Se intenta combinar la parte de roger con la rama Josue

---
+++ DAE/Interfaz/Autor.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DAE.Clases;
+
+namespace DAE.Interfaz
+{
+    public partial class frmAutor : Form
+    {
+        public frmAutor()
+        {
+            InitializeComponent();
+        }
+        ClsAutor au = new ClsAutor();
+        private void cargar()
+        {
+            dtAutor.DataSource = au.getDatos();
+        }
+        private void listarCategorias()
+        {
+            cbCategoria.DisplayMember = "NombreCategoria";
+            cbCategoria.ValueMember = "CodigoCategoria";
+            cbCategoria.DataSource = au.getDatos("listaCategorias");
+        }
+        private void frmAutor_Load(object sender, EventArgs e)
+        {
+            cargar();
+            listarCategorias();
+        }
+        private void limpiarCampos()
+        {
+            txtCodigo.Text = "";
+            txtNombre.Text = "";
+            cbCategoria.Text = "";
+            txtBuscar.Text = "";
+            cbOpcion.Text = "";
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                au.Nombre = txtNombre.Text;
+                au.Categoria = Convert.ToInt32(cbCategoria.SelectedValue);
+
+
+
+                au.insertarDatos(au);
+                limpiarCampos();
+                cargar();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("ocurrio un error al insertar:" + err.Message, "error!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dtAutor_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            this.txtCodigo.Text = dtAutor.SelectedRows[0].Cells[0].Value.ToString();
+            this.txtNombre.Text = dtAutor.SelectedRows[0].Cells[1].Value.ToString();
+            this.cbCategoria.Text = dtAutor.SelectedRows[0].Cells[2].Value.ToString();
+
+
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (txtCodigo.Text != "")
+            {
+                try
+                {
+                    au.Codigo = Convert.ToInt32(txtCodigo.Text);
+                    au.Nombre = txtNombre.Text;
+                    au.Categoria = Convert.ToInt32(cbCategoria.SelectedValue);
+
+
+                    au.modificarDatos(au);
+                    limpiarCampos();
+                    cargar();
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show("ocurrio un error al modificar:" + err.Message, "error!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (txtCodigo.Text != "")
+            {
+                string mensaje = "seguro que desea eliminar el contenido?";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                MessageBoxIcon icon = MessageBoxIcon.Question;
+                DialogResult result = MessageBox.Show(mensaje, "ELIMINANDO", buttons, icon);
+                if (result == DialogResult.Yes)
+                {
+                    au.eliminarDatos(txtCodigo.Text);
+                    cargar();
+                    limpiarCampos();
+                }
+            }
+        }
+
+        private void btnFiltrar_Click(object sender, EventArgs e)
+        {
+            if (txtBuscar.Text != "" && cbOpcion.Text != "")
+            {
+                string campo;
+                if (cbOpcion.Text == "Codigo")
+                {//validar opciones de busqueda
+                    campo = "CodigoAutor";
+
+                }
+                else if (cbOpcion.Text == "Nombre")
+                {
+                    campo = "NombreAutor";
+                }
+                else
+                {
+                    campo = "NombreCategoria";
+                }
+               
+                dtAutor.DataSource = au.buscarRegistro(campo, txtBuscar.Text);
+            }
+            else
+            {
+                string mensaje = "ingresar parametros de busqueda";
+                MessageBox.Show(mensaje, "informacion!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            cargar();
+            limpiarCampos();
+        }
+    }
+}
---
+++ DAE/Login.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DAE.Clases;
+using DAE.Interfaz;
+
+namespace DAE
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAcceder_Click(object sender, EventArgs e)
+        {
+            if (txtUser.Text != "") 
+            {
+                if (txtPassword.Text != "")
+                {
+                    ClsUsuario us = new ClsUsuario();
+                    //set a los atributos de la clase
+                    us.Usuario = txtUser.Text;
+                    us.Contra = txtPassword.Text;
+                    var validarLogin = us.getLogin();
+                    if (validarLogin == true)
+                    {
+                        FrmMenu menu = new FrmMenu();//instancia al formulario
+                        menu.Show();//mostrar formulario menu
+                        //sobrecargar el metodo FormClosed del formulario menu para cuando cerremos la sesion
+                        menu.FormClosed += LogOut;
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario o Contraseña incorrecta", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }   
+                }
+                else
+                {
+                    MessageBox.Show("La Contraseña es obligatoria!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("El Usuario es obligatorio!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        private void LogOut(object sender, FormClosedEventArgs e)
+        {
+            txtUser.Clear();
+            txtPassword.Clear();
+            this.Show();
+            txtUser.Focus();    
+        }
+
+        private void btnUserNotRegistred_Click(object sender, EventArgs e)
+        {
+            // Crear un nuevo usuario temporal
+            ClsUsuario us = new ClsUsuario();
+            UserLoginCache.userName = "Sin Usuario";
+            UserLoginCache.rolUser = "Temporal";
+
+            // Mostrar el formulario del menú
+            FrmMenu menu = new FrmMenu();
+            menu.Show();
+
+            // Ocultar el formulario de login
+            this.Hide();
+            menu.FormClosed += LogOut;
+        }
+
+        private void btnRegistrarUsuario_Click(object sender, EventArgs e)
+        {
+            frmUsuarios frm = new frmUsuarios();
+            frm.btnEditar.Enabled = false;
+            frm.btnEliminar.Enabled = false;
+            frm.TablaUsers.Visible = false;
+            frm.BuscarDatos.Visible = false;
+            frm.cmbCargoUsuario.Enabled = false;
+            frm.cmbCargoUsuario.Text = "Normal";
+            frm.btnRegresarLogin.Visible = true;
+            frm.txtContraseñaUsuario.PasswordChar = '\0';
+            frm.CrudUsers.Location = new Point(207, 179);
+            frm.Size = new Size(1075, 50);
+
+            frm.Show();
+        }
+    }
+}
